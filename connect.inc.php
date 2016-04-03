0ee8da8d8a2cc5e2f32afbd3f5ff898f9ecf0557
Travis D <travis.dacosta1@gmail.com>
2016-04-03 11:52:07 -0400

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,8 +1,10 @@
 <?php
 	//PDO Way	
-
+	//Create a database in phpmadmin and replace with database name in the dbname field
+	//Also if using a different host, username, or password just change the fields
+	
 	try {	
-		$link = new PDO('mysql:host=localhost; dbname=a_database', 'root', '');
+		$link = new PDO('mysql:host=localhost; dbname=...', 'root', '');
 		$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
 		ob_start();		
 		echo 'Connected<br><br>';
