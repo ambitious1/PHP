8c8c9b0890af5b8e10b0f5faae856412b6f52dc7
Travis D <travis.dacosta1@gmail.com>
2016-04-12 19:27:53 -0400

    CRUD website home page

---
+++ home.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<body>
+			<h1><center>
+				Welecome to CRUD control Center	
+			</center>			
+			</h1>		
+			<p>
+				<footer>
+					<center>
+						<?php include("links.php"); ?>					
+					</center>				
+				<footer>			
+			</p>
+		</body>	
+	</head>
+</html>
---
+++ links.php
@@ -0,0 +1,7 @@
+<p>
+	<a href="home.php">Home</a>&nbsp
+	<a href="insert/form.php">Register</a>&nbsp
+	<a href="update.php">Edit User</a>&nbsp
+	<a href="output/form.php">See Data</a>&nbsp
+	<a href="delete.php">Delete User</a>&nbsp
+</p>
