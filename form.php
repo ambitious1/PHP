f69c5bdf84ffdb2ec9bda3763c5636338ced1661
Travis D <travis.dacosta1@gmail.com>
2016-04-12 19:39:39 -0400

    CRUD website insert data

--- form.php
+++ form.php
@@ -1,27 +1,19 @@
 <html>
 	<head>
 		<body>
-			<h2>
-				Register Form			
-			</h2>	
-			
-			<form method="post" action="../insert/insert.php">
-				<table border="0" width="60%">
-					<tr><td width="10%">Name: </td><td><input type="text" name="name" ></td></tr>
-					<tr><td width="10%">Email: </td><td><input type="text" name="email" ></td></tr>
-					<tr><td width="10%">Password: </td><td><input type="password" name="password" ></td></tr>
-				</table>
-				
-				<input type="submit" value="Register"><br>				
-				
+			<form method="post" action="insert.php">
+				Name: <input type="text" name="name"><br>
+				Email: <input type="text" name="email"><br>
+				Password: <input type="password" name="password"><br>
+				<p>
+					<input type="submit" value="register"><br>				
+				</p>
 			</form>
-			
-			<a href="output.php">See Data</a>	
 			<footer>
 					<center>
 						<?php include("../links.php"); ?>					
 					</center>				
-			<footer>			
+			<footer>					
 		</body>	
 	</head>
 </html>
