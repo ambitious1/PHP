e9742f3f844efbeabfa7616d736dbff069ac929c
Travis D <travis.dacosta1@gmail.com>
2016-04-14 00:58:54 -0400

    CRUD Website insert Replacement with Validation
    Checks if username was chosen already. If so then output an error message

--- insert.php
+++ insert.php
@@ -1,19 +1,39 @@
 <?php
 
-require 'connect.inc.php';
+require '../connect.inc.php';
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+$username = $link->query("SELECT `name` FROM `people` WHERE `name`='$name'");
+$count = $username->rowCount($username);
+
+if ($count!=0){
+	
+	echo "<p>
+				<a href=\"../home.php\">Home</a>&nbsp
+				<a href=\"form.php\">Register</a>&nbsp
+				<a href=\"../edit/update.php\">Edit User</a>&nbsp
+				<a href=\"../output/index.php\">See Data</a>&nbsp
+				<a href=\"delete.php\">Delete User</a>&nbsp
+			</p>";
+				
+	die("<b>ERROR: Name already exists! Please type another name</b>");
+	
+}
+
+
 if($name && $email && $password) {
 	$query = $link->query("INSERT INTO people(name,email,password) VALUES ('$name', '$email','$password')");
-	//$query_run = $query->execute();
+	
+	echo "You have successfully registered!";
+		
 	while($registered = $query->fetchAll()){
 	if($registered){
-		echo '<pre>', print_r($registered), '</pre>';
-		echo 'You must complete the form';
+		echo '<pre>', print_r($registered), '</pre>';	
 	} else {
+			echo 'You must complete the form';
 		} 
 	}
 }
